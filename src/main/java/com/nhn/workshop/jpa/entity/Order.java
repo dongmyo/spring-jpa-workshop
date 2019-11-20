8c66f65a18a7fda6d9439dfeb2507ad5807e1812
신동민/NE <dongmyo@nhnent.com>
2019-11-20 19:45:01 +0900

    * bug fix

--- src/main/java/com/nhn/workshop/jpa/entity/Order.java
+++ src/main/java/com/nhn/workshop/jpa/entity/Order.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Table(name = "Orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "order_id")
     private Long orderId;
 
