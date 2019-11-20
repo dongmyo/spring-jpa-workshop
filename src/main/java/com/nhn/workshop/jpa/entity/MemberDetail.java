f6643b44f2aea8093a9666b742ac5499dc79c1a0
신동민/NE <dongmyo@nhnent.com>
2019-11-20 14:59:56 +0900

    * 실습 정답

--- src/main/java/com/nhn/workshop/jpa/entity/MemberDetail.java
+++ src/main/java/com/nhn/workshop/jpa/entity/MemberDetail.java
@@ -3,11 +3,14 @@ package com.nhn.workshop.jpa.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -21,6 +24,8 @@ public class MemberDetail {
     private Long memberDetailId;
 
     // TODO: 연관관계 설정
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     private String type;
